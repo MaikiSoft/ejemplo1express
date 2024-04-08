996c6a1ecd693aaca5861aaf7769c468e6dd817f
lola <lola@gmail.com>
2024-04-08 15:44:11 -0500

    Tercer Commit
    funciones get, put y delete

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -9,4 +9,29 @@ router.post("/animals", (req, res) => {
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
+router.get("/animals", (req, res) => {
+    animalSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+
+
+router.put("/animals/:id", (req, res) => {
+    animalSchema.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+router.delete("/animals/:id", (req, res) => {
+    const { id } = req.params;
+    animalSchema
+        .findByIdAndDelete(id)
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) => {
+            res.json({ message: error });
+        });
+});
+
 module.exports = router;
