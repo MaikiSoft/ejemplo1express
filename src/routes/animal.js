b507ef73baff855de726234ce80a8def9270c0b8
Maik <maicolestebancastillolopez@gmail.com>
2024-04-15 16:17:55 -0500

    quinto commit
    se agrego la autenticacion de usuario

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const animalSchema = require("../models/animal");
+const verifyToken = require('./validate_token');
+
 //Nuevo animal
 router.post("/animals", (req, res) => {
     const animal = animalSchema(req.body);
@@ -9,11 +11,17 @@ router.post("/animals", (req, res) => {
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
+router.get("/animals", verifyToken, (req,res) =>{
+    animalSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({message: error}));
+});
 router.get("/animals", (req, res) => {
     animalSchema.find()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
+
 
 
 
